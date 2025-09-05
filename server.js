743935b39ef11ef854fa7d7764b14eea7bd9fb4d
VinhNguyenSVG <nghbvinh2312@gmail.com>
2025-09-05 18:12:49 +0700

    PT-217: Create APIs calculate 2 numbers

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
-
+const calculate = require("./router/calculate");
 const port = 3000;
+
+app.use(express.json());
+app.use("/calculate", calculate);
 
 app.listen(port, () => {
   console.log(`server is running on http://localhost:${port}`);
